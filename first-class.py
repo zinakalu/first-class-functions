17b3706243b99d7faca9a767c35abb9963cdc8c7
zinakalu <zina.kalu@gmail.com>
2023-05-04 07:30:28 -0400

    first-class-functions-refresher

---
+++ first-class.py
@@ -0,0 +1,22 @@
+def square(x):
+    return x*x
+
+# f = square
+
+# print(square)
+# print(f(5))
+
+def cube(x):
+    return x*x*x
+
+def my_map(func, arg_list):
+    result = []
+    for i in arg_list:
+        result.append(func(i))
+    return result
+
+
+squares = my_map(cube, [1,2,3,4,5])
+
+print(squares)
+
